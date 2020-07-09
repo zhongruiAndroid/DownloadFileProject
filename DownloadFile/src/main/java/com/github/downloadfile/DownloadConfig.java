5a0c95469b522ed6f7f26700377c4ded134e5620
zhongrui <zhongrui@021.com>
2020-07-09 21:02:14 +0800

    完善下载config

--- DownloadFile/src/main/java/com/github/downloadfile/DownloadConfig.java
+++ DownloadFile/src/main/java/com/github/downloadfile/DownloadConfig.java
@@ -1,30 +1,151 @@
 package com.github.downloadfile;
 
-import android.app.AlertDialog;
+import android.content.Context;
+import android.os.Environment;
+import android.text.TextUtils;
 
+import java.io.File;
 import java.io.Serializable;
 
 public class DownloadConfig implements Serializable {
-    private String filePath;
-
-    private String fileName;
-    private String tempFileNameSuffix;
+    private File saveFile;
+    private File tempSaveFile;
+    private boolean ifExistAgainDownload;
+    private String fileDownloadUrl;
+    private boolean useSourceName;
 
     protected DownloadConfig() {
     }
 
-    public static class Builder{
+    public static class Builder {
+        private String downloadFilePath;
         private DownloadConfig config;
 
         public Builder() {
-            config=new DownloadConfig();
+            config = new DownloadConfig();
         }
-        AlertDialog bb=new AlertDialog.Builder(null).setTitle("").create();
 
-        public DownloadConfig build(){
-            DownloadConfig downloadConfig=new DownloadConfig();
+        public Builder(Context context) {
+            config = new DownloadConfig();
+            if (context == null) {
+                return;
+            }
+            boolean sdCardCanReadAndWrite = Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+            File useDownloadFile = context.getExternalFilesDir("download");
+            if (sdCardCanReadAndWrite && useDownloadFile != null) {
+                downloadFilePath = useDownloadFile.getAbsolutePath();
+            } else {
+                downloadFilePath = context.getFilesDir() + File.separator + "download";
+            }
+        }
+
+        public void setSaveFile(String filePath, String fileName) {
+            config.setSaveFile(filePath, fileName);
+        }
+
+        public void setSaveFile(File saveFile) {
+            config.setSaveFile(saveFile);
+        }
+
+        public void setIfExistAgainDownload(boolean ifExistAgainDownload) {
+            config.setIfExistAgainDownload(ifExistAgainDownload);
+        }
+
+        public void setFileDownloadUrl(String fileDownloadUrl) {
+            config.setFileDownloadUrl(fileDownloadUrl);
+        }
+
+        public void setUseSourceName(boolean useSourceName) {
+            config.setUseSourceName(useSourceName);
+        }
+
+        public DownloadConfig build() {
+            if (config.getSaveFile() == null) {
+                if (TextUtils.isEmpty(downloadFilePath)) {
+                    throw new IllegalStateException("please call setSaveFile() set filePath and fileName");
+                } else {
+                    String fileDownloadUrl = config.getFileDownloadUrl();
+                    if (TextUtils.isEmpty(fileDownloadUrl)) {
+                        throw new IllegalStateException("please call setFileDownloadUrl() set downloadUrl");
+                    }
+                    int index = fileDownloadUrl.lastIndexOf(".");
+                    String suffix = fileDownloadUrl.substring(index);
+                    String fileName = MD5Coder.encode(fileDownloadUrl) + suffix;
+                    if (config.isUseSourceName()) {
+                        index = fileDownloadUrl.lastIndexOf("/");
+                        fileName = fileDownloadUrl.substring(index);
+                    }
+                    config.setSaveFile(new File(downloadFilePath, fileName));
+                }
+            } else {
+                String fileDownloadUrl = config.getFileDownloadUrl();
+                if (config.isUseSourceName()&&!TextUtils.isEmpty(fileDownloadUrl)) {
+                    int index = fileDownloadUrl.lastIndexOf("/");
+                    String fileName = fileDownloadUrl.substring(index);
+                    config.setSaveFile(new File(config.getSaveFile().getParent(), fileName));
+                }
+            }
+            DownloadConfig downloadConfig = new DownloadConfig();
+
+            String name = config.getSaveFile().getName();
+            String substring = name.substring(0, name.lastIndexOf("."));
+
+            downloadConfig.setTempSaveFile(new File(config.getSaveFile().getParent(),substring+".temp"));
+            downloadConfig.setSaveFile(config.getSaveFile());
+            downloadConfig.setFileDownloadUrl(config.getFileDownloadUrl());
+            downloadConfig.setUseSourceName(config.isUseSourceName());
+            downloadConfig.setIfExistAgainDownload(config.isIfExistAgainDownload());
 
             return downloadConfig;
         }
     }
+
+    public File getSaveFile() {
+        return saveFile;
+    }
+
+    public void setSaveFile(String filePath, String fileName) {
+        setSaveFile(new File(filePath, fileName));
+    }
+
+    public void setSaveFile(File saveFile) {
+        this.saveFile = saveFile;
+    }
+
+    public File getTempSaveFile() {
+        return tempSaveFile;
+    }
+
+    public void setTempSaveFile(File tempSaveFile) {
+        this.tempSaveFile = tempSaveFile;
+    }
+
+    public boolean isIfExistAgainDownload() {
+        return ifExistAgainDownload;
+    }
+
+    public void setIfExistAgainDownload(boolean ifExistAgainDownload) {
+        this.ifExistAgainDownload = ifExistAgainDownload;
+    }
+
+    public String getFileDownloadUrl() {
+        return fileDownloadUrl;
+    }
+
+    public void setFileDownloadUrl(String fileDownloadUrl) {
+        this.fileDownloadUrl = fileDownloadUrl;
+    }
+
+    public boolean isUseSourceName() {
+        return useSourceName;
+    }
+
+    public void setUseSourceName(boolean useSourceName) {
+        this.useSourceName = useSourceName;
+    }
+
+    public boolean isExistFile(){
+        return getSaveFile()!=null&&getSaveFile().exists();
+    }
+
 }
--- app/src/test/java/com/test/downloadfileproject/ExampleUnitTest.java
+++ app/src/test/java/com/test/downloadfileproject/ExampleUnitTest.java
@@ -2,6 +2,8 @@ package com.test.downloadfileproject;
 
 import org.junit.Test;
 
+import java.io.File;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +15,25 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void asdf() {
+        String url="http://vfx.mtime.cn/Video/2019/03/19/mp4/190319222227698228.mp4";
+        int i = url.lastIndexOf(".");
+        String substring = url.substring(i);
+        System.out.println(url.hashCode());
+    }
+    @Test
+    public void asddf() {
+        File file=new File("f:/a/b/c");
+        System.out.println(file.getName());
+        System.out.println(file.getPath());
+        System.out.println(file.getAbsolutePath());
+        String absolutePath = file.getAbsolutePath();
+        int i = absolutePath.lastIndexOf("\\");
+        String substring = absolutePath.substring(i  );
+        String substring2 = absolutePath.substring(i);
+        System.out.println(substring);
+        System.out.println(substring2);
     }
 }
